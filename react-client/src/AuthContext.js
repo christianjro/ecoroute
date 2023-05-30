83310782a78ec5fbb7a509c97fab0246b49a3f28
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-05-30 13:35:59 -0700

    refactored AuthContext and consolidated logic

--- react-client/src/AuthContext.js
+++ react-client/src/AuthContext.js
@@ -1,10 +1,35 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import Cookie from 'js-cookie';
 
-const AuthContext = createContext({
+export const AuthContext = createContext({
     token: null, 
     isLoggedIn: false,
-    setToken: () => {},
-    setIsLoggedIIn: () => {}
+    loginUser: (token) => {},
+    logoutUser: () => {}
 });
 
-export default AuthContext;
+export default function AuthContextProvider({children}) {
+    const [token, setToken] = useState(Cookie.get("token") || null)
+    const [isLoggedIn, setIsLoggedIn] = useState(token !== null)
+
+    function loginUser(token) {
+        Cookie.set('token', token)
+        setToken(token)
+        setIsLoggedIn(true) 
+    }
+
+    function logoutUser() {
+        Cookie.remove('token')
+        setToken(null)
+        setIsLoggedIn(false)
+    }
+
+    const value = {
+        token: token,
+        isLoggedIn: isLoggedIn,
+        loginUser: loginUser,
+        logoutUser: logoutUser
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </BrowserRouter>
   </React.StrictMode>
 );
